0d936c169a512060b66b66659b6a6e2e611154c7
Jarrett Cruger <jcrugzz@gmail.com>
2014-03-01 02:46:16 -0500

    [bin] bin script

---
+++ bin/npm-ev-source.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+var EventSource = require('../event-source');
+
+var yargs = require('yargs')
+  .usage('$0 [options]')
+  .options('c', {
+    description: 'Config file to encapsulate all the things if you roll like that',
+    alias: 'config',
+    string: true,
+  })
+  .options('r', {
+    description: 'Registry where you specifically fetch TARBALLs from',
+    alias: 'registry',
+    string: true
+  })
+  .options('e', {
+    description: 'Event source couchdb database to insert docs into',
+    alias: 'event-source',
+    string: true
+  })
+  .options('s', {
+    description: 'Skimdb to listen on changes for to run this process',
+    alias: 'skim',
+    string: true
+  })
+  .options('t', {
+    description: 'Temp directory to store the tarballs that are fetched',
+    alias: 'tmp',
+    string: true
+  })
+  .options('u', {
+    description: 'user-agent if you want to specify your own',
+    alias: 'user-agent',
+    string: true
+  })
+  .options('h', {
+    description: 'Displays this message',
+    alias: 'h'
+  })
+
+var argv = yargs.argv;
+
+if (argv.h) {
+  return yargs.showHelp();
+}
+
+var config = argv.c
+  ? require(path.resolve(argv.c))
+  : { registry: argv.r, eventSource: argv.e, skim: argv.s, ua: argv.u, tmp: argv.t }
+
+try {
+  var es = new EventSource(config)
+}
+catch (ex) {
+  console.error(ex);
+  yargs.showHelp();
+  process.exit(1);
+}
+
+es.on('start', function () {
+  console.log('AND WE BEGIN GOOD SIR/MADAM %s pid=%d', es.ua, process.pid);
+}).on('change', function (change) {
+  console.log('CHANGE %d: %s', change.seq, change.id);
+}).on('put', function (doc) {
+  console.log('PUT <- doc %s', doc._id);
+}).on('delete', function (doc) {
+  console.log('DELETE doc %s', doc._id);
+}).on('error', function (err) {
+  console.error(err);
+  throw err;
+}).on('download', function(name) {
+  console.log('DOWNLOAD -> doc %s', name);
+})
